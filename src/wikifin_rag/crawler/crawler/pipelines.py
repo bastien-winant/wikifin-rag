eb930e3e2e1c3e1134e0561cf7fe4d9132f5d9e8
Bastien Winant <armand.winant@gmail.com>
2026-08-16 00:16:29 +0200

    SQL string composition

--- src/wikifin_rag/crawler/crawler/pipelines.py
+++ src/wikifin_rag/crawler/crawler/pipelines.py
@@ -1,6 +1,6 @@
 from itemadapter import ItemAdapter
 from wikifin_rag.ingest import get_db_connection
-
+from psycopg import sql
 
 class SQLiteUploadPipeline:
     collection_name = "document_chunks"
@@ -8,30 +8,39 @@ class SQLiteUploadPipeline:
     def __init__(self):
         self.con = get_db_connection()
         self.cur = self.con.cursor()
+        self.table_identifier = sql.Identifier(self.collection_name)
 
     def open_spider(self):
         self.cur.execute("CREATE EXTENSION IF NOT EXISTS vector;")
 
         self.cur.execute(
-            f"""CREATE TABLE IF NOT EXISTS {self.collection_name} (
-                chunk_id TEXT PRIMARY KEY,
-                source_url TEXT NOT NULL,
-                language TEXT,
-                date TEXT,
-                category TEXT,
-                description TEXT,
-                title TEXT,
-                html TEXT,
-                content TEXT,
-                embedding vector(768),
-                related_links TEXT[]
-            );"""
+            sql.SQL(
+                """
+                CREATE TABLE IF NOT EXISTS {} (
+                    chunk_id TEXT PRIMARY KEY,
+                    source_url TEXT NOT NULL,
+                    language TEXT,
+                    date TEXT,
+                    category TEXT,
+                    description TEXT,
+                    title TEXT,
+                    html TEXT,
+                    content TEXT,
+                    embedding vector(768),
+                    related_links TEXT[]
+                );
+                """
+            ).format(self.table_identifier)
         )
 
-        self.cur.execute(f"""
-            CREATE INDEX ON {self.collection_name}
-            USING hnsw (embedding vector_cosine_ops)
-        """)
+        self.cur.execute(
+            sql.SQL(
+                """
+                CREATE INDEX ON {}
+                USING hnsw (embedding vector_cosine_ops)
+                """
+            ).format(self.table_identifier)
+        )
 
     def close_spider(self):
         self.con.commit()
@@ -40,12 +49,15 @@ class SQLiteUploadPipeline:
 
     def process_item(self, item):
         adapter = ItemAdapter(item)
-        
+
         self.cur.execute(
-            f"""INSERT INTO {self.collection_name} (chunk_id, source_url, language, date, category, description, title, html, content, related_links)
+            sql.SQL(
+                """
+                INSERT INTO {} (chunk_id, source_url, language, date, category, description, title, html, content, related_links)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                 ON CONFLICT (chunk_id) DO NOTHING;
-            """,
+                """
+            ).format(self.table_identifier),
             (
                 adapter.get("chunk_id"),
                 adapter.get("source_url"),
