9e4d786a1fc4259a11e9e972d05ab3a9f54e6063
Bastien Winant <armand.winant@gmail.com>
2026-09-24 12:14:21 +0200

    simplify feedback logic

--- src/wikifin_rag/apps/app.py
+++ src/wikifin_rag/apps/app.py
@@ -4,60 +4,66 @@ from wikifin_rag.assistant import create_assistant
 from wikifin_rag.db_client import MonitoringDBClient
 from wikifin_rag.judge import evaluate_relevance
 
-
+db_client = MonitoringDBClient()
 assistant = create_assistant()
 
-db_client = MonitoringDBClient()
-db_client.init_db()
 
-
-def save_feedback():
+def save_feedback(index):
     conversation_id = st.session_state.conversation_id
-    db_client.save_feedback(conversation_id, "user", score=st.session_state[f"feedback_{conversation_id}"])
-    st.session_state.feedback_submitted = True
+    st.session_state.history[index]["feedback"] = st.session_state[f"feedback_{conversation_id}"]
+    db_client.save_feedback(
+        conversation_id=conversation_id,
+        source="user",
+        score=st.session_state[f"feedback_{conversation_id}"]
+    )
 
+
+if "history" not in st.session_state:
+    st.session_state.history = []
 
 st.title("Ask Wik:blue[i]f:green[i]n")
-container = st.container(border=True, height=330)
 
+for i, message in enumerate(st.session_state.history):
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+        if message["role"] == "assistant":
+            feedback = message.get("feedback", None)
+            st.session_state[f"feedback_{i}"] = feedback
+            st.feedback(
+                "thumbs",
+                key=f"feedback_{i}",
+                disabled=feedback is not None,
+                on_change=save_feedback,
+                args=[i],
+            )
 
-if new_prompt := st.chat_input("Say something"):
-    st.session_state.prompt = new_prompt
-    st.session_state.response = None
-    st.session_state.feedback_submitted = False
-
-
-prompt = st.session_state.get("prompt")
-
-if prompt:
-    container.chat_message("user").write(prompt)
-
-    response = st.session_state.get("response")
-    conversation_id = st.session_state.get("conversation_id")
-
-    with container.chat_message("assistant"):
-        if not response:
-            with st.spinner("Wait for it...", show_time=True):
-                # generate a response from the model
-                response = assistant.rag(prompt)
-                st.session_state.response = response
-
-                # save the model trace
-                record = assistant.last_call
-                conversation_id = db_client.save_conversation(record, prompt)
-                st.session_state.conversation_id = conversation_id
-
-                # generate and save LLM-as-judge feedback
-                relevance, explanation = evaluate_relevance(prompt, response)
-                db_client.save_feedback(conversation_id, "judge", relevance=relevance, explanation=explanation)
-
+if prompt := st.chat_input("Say something"):
+    st.chat_message("user").write(prompt)
+    st.session_state.history.append({"role": "user", "content": prompt})
     
-        st.write(response)
+    with st.chat_message("assistant"):
+        with st.spinner():
+            response = assistant.rag(prompt)
 
-        # listen for user feedback
-        st.feedback(
-            "thumbs",
-            key=f"feedback_{conversation_id}",
-            on_change=save_feedback,
-            disabled=st.session_state.get("feedback_submitted", False)
-        )
+            # save LLM response trace
+            record = assistant.last_call
+            conversation_id = db_client.save_conversation(record, prompt)
+            st.session_state.conversation_id = conversation_id
+
+            # LLM-as-a-judge
+            relevance, explanation = evaluate_relevance(prompt, response)
+            db_client.save_feedback(
+                conversation_id=conversation_id,
+                source="judge",
+                relevance=relevance,
+                explanation=explanation
+            )
+            
+            st.write(response)
+            st.feedback(
+                "thumbs",
+                key=f"feedback_{conversation_id}",
+                on_change=save_feedback,
+                args=[len(st.session_state.history)],
+            )
+    st.session_state.history.append({"role": "assistant", "content": response})
